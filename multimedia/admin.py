a3ba49df4da81b6933f0a5d955e76f5ba16c6aea
Helmy Giacoman <helmygb@gmail.com>
2010-10-05 23:37:18 -0600

    tiny_mce

--- multimedia/admin.py
+++ multimedia/admin.py
@@ -1,8 +1,17 @@
 from django.contrib import admin
 from models import *
 
-admin.site.register(Informacion)
-admin.site.register(Tema)
+class ModelOptions(admin.ModelAdmin):
+    class Media:
+        css = {
+            "all": ("/files/css/textarea.css", )
+        }
+
+        js = ('/files/js/tiny_mce/tiny_mce.js',
+              '/files/js/tiny_mce/tconfig.js')
+
+admin.site.register(Informacion, ModelOptions)
+admin.site.register(Tema, ModelOptions)
 admin.site.register(Subtema)
 admin.site.register(Genero)
 admin.site.register(Coleccion)
