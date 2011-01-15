6e0a018e361a32a7992a6905050f013894468cf3
Helmy Giacoman <helmygb@gmail.com>
2011-01-14 21:22:52 -0600

    terminando campañas

--- proyectos/urls.py
+++ proyectos/urls.py
@@ -3,4 +3,6 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns('luciernaga.proyectos.views',
     (r'^proyectos/$', 'proyectos'),
     (r'^proyectos/(?P<slug>[-\w]+)/$', 'proyecto_detail'),
+    (r'^campanias/$', 'campanias'),
+    (r'^campanias/(?P<slug>[-\w]+)/$', 'campania_detail'),
 )
--- proyectos/views.py
+++ proyectos/views.py
@@ -25,3 +25,23 @@ def proyecto_detail(request, slug):
 
     return render_to_response('proyectos/proyecto_detail.html', RequestContext(request, locals()))
 
+def campanias(request):
+    flag = 'proyecto'
+    temas = Tema.objects.filter(especifico=True)
+    temasall = Tema.objects.all()
+    try:
+        info = Informacion.objects.get(slug='campanias')
+    except:
+        pass
+
+    proyectos = Campania.objects.all()
+    return render_to_response('proyectos/campanias.html', RequestContext(request, locals()))
+
+def campania_detail(request, slug):
+    flag = 'proyecto'
+    temas = Tema.objects.filter(especifico=True)
+    temasall = Tema.objects.all()
+    proyecto = get_object_or_404(Campania, slug=slug)
+
+    return render_to_response('proyectos/campania_detail.html', RequestContext(request, locals()))
+
