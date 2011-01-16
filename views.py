f49001315d9c443702f2b89e266dfed702b44def
Helmy Giacoman <helmygb@gmail.com>
2011-01-16 01:19:04 -0600

    casi lo ultimo

--- views.py
+++ views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from luciernaga.eventos.models import*
+import datetime
+
 
 from luciernaga.multimedia.models import *
 
@@ -7,6 +10,9 @@ def index(request):
     temas = Tema.objects.filter(especifico=True)
     temasall = Tema.objects.all()
     videos_cant = Video.objects.all().count()
+    next = datetime.datetime.today() + datetime.timedelta(1 * 365 / 12)
+
+    last_two = Evento.objects.all()[:2]
     try:
         intro = Informacion.objects.get(slug='intro')
     except:
