4c4bfc36187b95c21c766381d19ee086439afa87
Helmy Giacoman <helmygb@gmail.com>
2011-02-09 23:02:12 -0600

    align

--- material/models.py
+++ material/models.py
@@ -7,10 +7,10 @@ from luciernaga.thumbs import ImageWithThumbsField
 
 class Documento(models.Model):
     titulo = models.CharField(max_length=150)
-    portada = ImageWithThumbsField(upload_to='documentos/thumbs', sizes=((80, 52), (112, 158), (140, 135), ), help_text='Portada del documento.')
+    portada = ImageWithThumbsField(upload_to='documentos/thumbs', sizes=((80, 52), (112, 158), (140, 135),), help_text='Portada del documento.')
     descripcion = models.TextField(blank=True, default='', help_text='Descripción corta del video')
     fecha = models.DateTimeField(default=datetime.datetime.now())
-    archivo = RestrictedFileField(upload_to='documentos', content_types=['application/pdf', ], max_upload_size=5242880, help_text='Formato PDF, Tamaño máx. 50MB')
+    archivo = RestrictedFileField(upload_to='documentos', content_types=['application/pdf',], max_upload_size=5242880, help_text='Formato PDF, Tamaño máx. 50MB')
     tema = models.ManyToManyField(Tema)
     videos_relacionados = models.ManyToManyField(Video, blank=True, verbose_name='Videos relacionados (max. 3)')
 
@@ -30,3 +30,7 @@ class FotoPortada(models.Model):
 
     class Meta:
         ordering = ['-id']
+
+class GenericVideo(models.Model):
+    titulo = models.CharField(max_length=150)
+    url = models.CharField(max_length=300, help_text='Introduzca la URL del video. Ejm: http://www.youtube.com/watch?v=rEi6Me2K2RY')
