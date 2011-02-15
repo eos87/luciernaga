3db4ae0a9f7205e52cfb37eba7ffbd1f742aa618
Helmy Giacoman <helmygb@gmail.com>
2011-02-15 01:22:06 -0600

    como quita tiempo esto

--- views.py
+++ views.py
@@ -9,14 +9,33 @@ import datetime
 from luciernaga.multimedia.models import *
 
 def index(request):
-    y = request.GET.get('y', '')
-    m = request.GET.get('m', '')
+    fecha = datetime.datetime.now()
     temas = Tema.objects.filter(especifico=True)
     temasall = Tema.objects.all()
     videos_cant = Video.objects.all().count()
     next = datetime.datetime.today() + datetime.timedelta(1 * 365 / 12)
-    c = True   
-    last_two = Evento.objects.all()[:2]
+       
+    eventos = Evento.objects.filter(fecha__month=fecha.month, fecha__year=fecha.year, fecha__gte=fecha).order_by('fecha')[:2]
+    proximos = Evento.objects.filter(fecha__month=fecha.month+1, fecha__year=fecha.year).order_by('fecha')[:2]
+    vacio = 0
+    flag = 0    
+    divuno_label = fecha
+    divdos_label = datetime.date(fecha.year, fecha.month+1, fecha.day)
+    if not eventos:
+        fecha2 = datetime.date(fecha.year, fecha.month+2, fecha.day)
+        eventos = Evento.objects.filter(fecha__month=fecha.month+1, fecha__year=fecha.year).order_by('fecha')[:2]
+        proximos = Evento.objects.filter(fecha__gte=fecha2).order_by('fecha')[:2]
+        divuno_label = divdos_label
+        divdos_label = fecha2
+    if not proximos and eventos:
+        fecha2 = datetime.date(fecha.year, fecha.month+2, 1)
+        proximos = Evento.objects.filter(fecha__gte=fecha2).order_by('fecha')[:2]
+        flag = 1
+    if not eventos:
+        eventos = Evento.objects.filter(fecha__gte=fecha).order_by('fecha')[:2]
+        proximos = []
+        flag = 2
+        vacio = 1
     try:
         index = FotoPortada.objects.get(slug='index')
     except: pass
@@ -27,8 +46,6 @@ def index(request):
         pass
     return render_to_response('index.html', RequestContext(request, locals()))
 
-from haystack.query import SearchQuerySet
 
-def CustomSearch(request):    
-
+def CustomSearch(request):
     return render_to_response('search/search.html', RequestContext(request, locals()))
